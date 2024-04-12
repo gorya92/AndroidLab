bd3a7bd3e4a285e04c8523703e9d34cf30535c5d
gorya92 <pospielov.sasha.01@mail.ru>
2024-04-12 13:14:43 +0300

    red

--- features/onboarding/src/androidTest/java/app/web/drjackycv/splash/ExampleInstrumentedTest.kt
+++ features/onboarding/src/androidTest/java/app/web/drjackycv/splash/ExampleInstrumentedTest.kt
@@ -20,5 +20,6 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("app.web.drjackycv.splash.test", appContext.packageName)
+
     }
 }
